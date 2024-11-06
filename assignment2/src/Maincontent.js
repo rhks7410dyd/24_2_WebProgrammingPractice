676aa162043e39115ca411b6f3e7b2f7561b82d8
Minpepha <rhks7410dyd@naver.com>
2024-11-06 16:47:31 +0000

    in academy

--- assignment2/src/Maincontent.js
+++ assignment2/src/Maincontent.js
@@ -4,7 +4,7 @@ import Subtitle from './Subtitletypo';
 import Para  from './Paragraphytypo';
 
 const MainContent = () => (
-  <div className="flex-grow bg-white p-16">
+  <div className="flex-grow bg-white px-[15%] pt-16">
     <Title innerText="Assignment" />
     <Para innerText="You need to zip the following files and submit zip file on iCampus." />
   </div>
